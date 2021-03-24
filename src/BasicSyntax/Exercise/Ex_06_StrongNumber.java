11948c2926e55d712d48c1e3d97983f05e5ddc17
Vesel Atanasov <77515052+VeselAtanasov@users.noreply.github.com>
2021-03-24 09:10:45 +0200

    Initial commit

---
+++ src/BasicSyntax/Exercise/Ex_06_StrongNumber.java
@@ -0,0 +1,12 @@
+package BasicSyntax.Exercise;
+
+import java.util.Scanner;
+
+public class Ex_06_StrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
